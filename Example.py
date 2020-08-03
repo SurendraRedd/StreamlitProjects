149a6c0575b57809bfa54ba15e9030133b107970
Surendra Reddy <37290660+SurendraRedd@users.noreply.github.com>
2020-08-03 10:54:40 +0530

    Update Example.py

--- Example.py
+++ Example.py
@@ -3,13 +3,13 @@ import urllib.request
 from PIL import Image
 import time
 
-def video_youtube(src: str, width="100%", height=315):
+def videoUserDefined(src: str, width="100%", height=315):
     """An extension of the video widget
     Arguments:
-        src {str} -- A youtube url like https://www.youtube.com/embed/B2iAodr0fOo
+        src {str} -- url of the video Eg:- https://www.youtube.com/embed/B2iAodr0fOo
     Keyword Arguments:
-        width {str} -- The width of the video (default: {"100%"})
-        height {int} -- The height of the video (default: {315})
+        width {str} -- video width(By default: {"100%"})
+        height {int} -- video height (By default: {315})
     """
     st.write(
         f'<iframe width="{width}" height="{height}" src="{src}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
@@ -19,11 +19,11 @@ def video_youtube(src: str, width="100%", height=315):
 def main():
 
     # Test/Title
-    st.title('Learning the Streamlit')
+    st.title('StreamLit basic Concepts')
 
     # Header/Subheader
-    st.header("This is a header")
-    st.subheader("This is a subheader")
+    st.header("To Create Header")
+    st.subheader("To Create subheader")
 
     #text
     st.text("hello Streamlit")
@@ -31,29 +31,31 @@ def main():
     #Markdown
     st.markdown("### This is a Markdown")
 
-    #Error/Colorful text
+    #Will be used for displaying the Error Messages in a colourful format
     st.success("Successful")
-
     st.info("Information!")
-
     st.warning('this is a warning')
-
     st.error("this is an error Danger")
+    
+    # Exception handling
+    st.exception("IndexError('list out of index')")
 
-    st.exception("NameError('name three not defined')")
-
+    #help of the function
     st.help(range)
 
     st.write("Text with write")
 
     st.write(range(10))
 
-    # Images
-    img = Image.open(urllib.request.urlopen("https://mms.businesswire.com/media/20200616005364/en/798639/22/Streamlit_Logo_%281%29.jpg"))
+    #Image opening
+    img = Image.open("download.jfif") #open the image stored in specified location
+    #img = Image.open(urllib.request.urlopen("https://mms.businesswire.com/media/20200616005364/en/798639/22/Streamlit_Logo_%281%29.jpg")) # Opens the image from the url
     st.image(img, width=300, caption="Simple Image")
 
-    # Videos
-    video_youtube("https://www.youtube.com/embed/B2iAodr0fOo")
+    # Video playing
+    vid_file = open("sample-mp4-file.mp4","rb").read() #play the video stored in specified location
+    st.video(vid_file)
+    #videoUserDefined("https://www.youtube.com/embed/B2iAodr0fOo")
 
     #widgets
     if st.checkbox("Show/hide"):
@@ -70,13 +72,14 @@ def main():
         ('Home', 'About', 'Help')
     )
 
+    # Sample Progress bar
     my_bar = st.progress(0)
 
     for percent_complete in range(100):
         time.sleep(0.1)
         my_bar.progress(percent_complete + 1)
 
-
+    
 ##########################################################################
 # @brief Entry point of the python
 ##########################################################################
