b12e00f15e27e00eca7c83c037859df8c3d36b86
Surendra Reddy <37290660+SurendraRedd@users.noreply.github.com>
2020-08-30 07:29:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
-mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"applicationdevelopment2018@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+mkdir -p ~/.streamlit
+
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
